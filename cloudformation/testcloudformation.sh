3c8491f4d71bce70ab8e418a41275d26129675d1
larse514 <andrew.larsen@concordusa.com>
2018-02-28 21:03:51 -0600

    add vpc

--- cloudformation/testcloudformation.sh
+++ cloudformation/testcloudformation.sh
@@ -20,8 +20,10 @@ WSSubnet1=`aws cloudformation describe-stacks --stack-name "${vpcname}" --query 
 WSSubnet2=`aws cloudformation describe-stacks --stack-name "${vpcname}" --query "Stacks[0].[Outputs[? starts_with(OutputKey, 'WSSubnet2')]][0][*].{OutputValue:OutputValue}" --output text`
 WSSubnet3=`aws cloudformation describe-stacks --stack-name "${vpcname}" --query "Stacks[0].[Outputs[? starts_with(OutputKey, 'WSSubnet3')]][0][*].{OutputValue:OutputValue}" --output text`
 
+VPC=`aws cloudformation describe-stacks --stack-name "${vpcname}" --query "Stacks[0].[Outputs[? starts_with(OutputKey, 'VPC')]][0][*].{OutputValue:OutputValue}" --output text`
+
 aws cloudformation package --template-file cloudformation/ecs.yml --output-template-file ecs-output.yml --s3-bucket circleci.deployables
-aws cloudformation deploy --template-file ecs-output.yml --capabilities CAPABILITY_IAM --stack-name "${ecstemplate}" --parameter-overrides KeyName="" VpcId=vpc-c7aa77be WSSubnetId="${WSSubnet1},${WSSubnet2},${WSSubnet1}" ClusterSubnetId="${AppSubnet1},${AppSubnet2},${AppSubnet3}" InstanceType=t2.medium DomainName=vssdevelopment.com
+aws cloudformation deploy --template-file ecs-output.yml --capabilities CAPABILITY_IAM --stack-name "${ecstemplate}" --parameter-overrides KeyName="" VpcId=${VPC} WSSubnetId="${WSSubnet1},${WSSubnet2},${WSSubnet1}" ClusterSubnetId="${AppSubnet1},${AppSubnet2},${AppSubnet3}" InstanceType=t2.medium DomainName=vssdevelopment.com
 
 #Get output values, this is a soemwhat naive approach since it is a lot of api calls
 ecscluster=`aws cloudformation describe-stacks --stack-name "${ecstemplate}" --query "Stacks[0].[Outputs[? starts_with(OutputKey, 'ecscluster')]][0][*].{OutputValue:OutputValue}" --output text`
