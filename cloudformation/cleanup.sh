e0569a869805be91a70dcb48840fd47914ae9c95
larse514 <andrew.larsen@concordusa.com>
2018-02-28 21:01:22 -0600

    add VPC creation

--- cloudformation/cleanup.sh
+++ cloudformation/cleanup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 ecstemplate=$1
 container=$2
+image=$3
+vpcname=$4
 
 aws cloudformation delete-stack --stack-name "${container}"
 
 aws cloudformation delete-stack --stack-name "${ecstemplate}"
+
+aws cloudformation delete-stack --stack-name "${vpcname}"
--- cloudformation/testcloudformation.sh
+++ cloudformation/testcloudformation.sh
@@ -5,9 +5,23 @@ set -e
 ecstemplate=$1
 container=$2
 image=$3
+vpcname=$4
+
+
+aws cloudformation package --template-file vpc.yml --output-template-file vpc-output.yml --s3-bucket moodle-deployables
+aws cloudformation deploy --template-file vpc-output.yml --capabilities CAPABILITY_IAM --stack-name "${vpcname}" \
+--parameter-overrides VPCName="${vpcname}" Tenant=amazonian
+
+AppSubnet1=`aws cloudformation describe-stacks --stack-name "${vpcname}" --query "Stacks[0].[Outputs[? starts_with(OutputKey, 'APPSubnet1')]][0][*].{OutputValue:OutputValue}" --output text`
+AppSubnet2=`aws cloudformation describe-stacks --stack-name "${vpcname}" --query "Stacks[0].[Outputs[? starts_with(OutputKey, 'APPSubnet2')]][0][*].{OutputValue:OutputValue}" --output text`
+AppSubnet3=`aws cloudformation describe-stacks --stack-name "${vpcname}" --query "Stacks[0].[Outputs[? starts_with(OutputKey, 'APPSubnet3')]][0][*].{OutputValue:OutputValue}" --output text`
+
+WSSubnet1=`aws cloudformation describe-stacks --stack-name "${vpcname}" --query "Stacks[0].[Outputs[? starts_with(OutputKey, 'WSSubnet1')]][0][*].{OutputValue:OutputValue}" --output text`
+WSSubnet2=`aws cloudformation describe-stacks --stack-name "${vpcname}" --query "Stacks[0].[Outputs[? starts_with(OutputKey, 'WSSubnet2')]][0][*].{OutputValue:OutputValue}" --output text`
+WSSubnet3=`aws cloudformation describe-stacks --stack-name "${vpcname}" --query "Stacks[0].[Outputs[? starts_with(OutputKey, 'WSSubnet3')]][0][*].{OutputValue:OutputValue}" --output text`
 
 aws cloudformation package --template-file cloudformation/ecs.yml --output-template-file ecs-output.yml --s3-bucket circleci.deployables
-aws cloudformation deploy --template-file ecs-output.yml --capabilities CAPABILITY_IAM --stack-name "${ecstemplate}" --parameter-overrides KeyName="" VpcId=vpc-c7aa77be SubnetId=subnet-b61d81fe,subnet-0202dc58 InstanceType=t2.medium DomainName=vssdevelopment.com
+aws cloudformation deploy --template-file ecs-output.yml --capabilities CAPABILITY_IAM --stack-name "${ecstemplate}" --parameter-overrides KeyName="" VpcId=vpc-c7aa77be WSSubnetId="${WSSubnet1},${WSSubnet2},${WSSubnet1}" ClusterSubnetId="${AppSubnet1},${AppSubnet2},${AppSubnet3}" InstanceType=t2.medium DomainName=vssdevelopment.com
 
 #Get output values, this is a soemwhat naive approach since it is a lot of api calls
 ecscluster=`aws cloudformation describe-stacks --stack-name "${ecstemplate}" --query "Stacks[0].[Outputs[? starts_with(OutputKey, 'ecscluster')]][0][*].{OutputValue:OutputValue}" --output text`
