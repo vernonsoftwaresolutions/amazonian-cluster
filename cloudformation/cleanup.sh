5f54b748a1ceea243441e8ab43640bb16f864f3a
larse514 <andrew.larsen@concordusa.com>
2018-03-03 19:22:43 -0600

    add ecs cluster dashboard

--- cloudformation/cleanup.sh
+++ cloudformation/cleanup.sh
@@ -5,6 +5,7 @@ image=$3
 vpcname=$4
 
 aws cloudformation delete-stack --stack-name "${container}"
+aws cloudformation delete-stack --stack-name "${container}2"
 
 aws cloudformation delete-stack --stack-name "${ecstemplate}"
 
